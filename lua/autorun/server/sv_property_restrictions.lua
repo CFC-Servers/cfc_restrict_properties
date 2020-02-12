0d53f6a79b22d090ef3760b9eea88bd8329a2fbf
Claydo9 <54473393+Claydo9@users.noreply.github.com>
2020-02-12 12:27:28 +1000

    Update lua/autorun/server/sv_property_restrictions.lua
    Co-Authored-By: Brandon Sturgeon <sturgeonb4@gmail.com>

--- lua/autorun/server/sv_property_restrictions.lua
+++ lua/autorun/server/sv_property_restrictions.lua
@@ -10,7 +10,7 @@
 
 
 --/\-- Hooks
-hook.Remove("CanProperty", "persist_property")
+hook.Remove( "CanProperty", "CFC_RestrictPersist" )
 
 hook.Add( "CanProperty", "persist_property", function( ply, property, ent )
 	if ( !ply:IsAdmin() && property == "persist" ) then return false else return true end
